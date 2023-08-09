f12e243d3ab164bcf3423d33c76aff0547d9f00d
marivdau <87074474+marivdau@users.noreply.github.com>
2023-08-09 22:23:36 +0300

    completed hometask 33

--- lesson33/script.js
+++ lesson33/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+//Lesson 33
 const users = [
   {
     id: 1,
@@ -30,3 +30,48 @@ console.log(users);
 const desireAge = 22;
 const existingUserWithAge = users.some((element) => element.age === desireAge)
 console.log(`Do we have a user with age = 22? Answer: ${existingUserWithAge}`);
+
+//Homework 33
+const subjects = {
+  mathematics: {
+    students: 200,
+    teachers: 6
+  },
+  biology: {
+    students: 120,
+    teachers: 6
+  },
+  geography: {
+    students: 60,
+    teachers: 2
+  },
+  chemistry: {
+    students: 100,
+    teachers: 3
+  }
+}
+
+//1
+const subjectsName = Object.keys(subjects).join();
+console.log(`Список предметов: ${subjectsName}`);
+
+const studentsAmount = Object.values(subjects).reduce((accumulator, currentValue) => accumulator + currentValue.students, 0)
+console.log(`Общее количество студентов: ${studentsAmount}`)
+const teachersAmount = Object.values(subjects).reduce((accumulator, currentValue) => accumulator + currentValue.teachers, 0)
+console.log(`Общее количество учителей: ${teachersAmount}`)
+//3
+const averageStudentsNumber = studentsAmount / Object.keys(subjects).length;
+console.log(`Среднее число студентов ${averageStudentsNumber}`)
+//4
+const arrayToPush = [];
+for (let i in subjects) {
+  arrayToPush.push(subjects[i]);
+}
+const arrayOfObjects = JSON.stringify(arrayToPush);
+console.log(`Массив из объектов предметов: ${arrayOfObjects}`);
+//5
+const sorted = Object.fromEntries(
+  Object.entries(subjects).sort(([, a], [, b]) => b.teachers - a.teachers)
+)
+const subjectsListTeachersNumberDesc = Object.keys(sorted);
+console.log(`Массив из предметов сортированный по количеству преподавателей от большего к меньшему: ${subjectsListTeachersNumberDesc}`)
